9d16a925c482caef5511df8fd208ec0015ac178a
Mudassir 💥 <127849625+mudassir-725@users.noreply.github.com>
2025-11-10 17:47:24 +0530

    Delete server v1.8.8 directory

--- server/src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-// src/middleware/errorHandler.js
-export function errorHandler(err, req, res, next) {
-    console.error("🔥 Error:", err.message);
-    res.status(err.status || 500).json({
-        success: false,
-        message: err.message || "Internal Server Error"
-    });
-}
